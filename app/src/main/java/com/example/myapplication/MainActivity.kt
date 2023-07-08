bb299e981dd16cd84ecf3be6993b62e320195470
Haggai Khafizov <hafyok5777@gmail.com>
2023-07-08 15:46:19 +0300

    Card, Box and Image

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,49 +3,68 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row (modifier = Modifier
-                .background(Color.Gray).fillMaxWidth().fillMaxHeight(0.5f)){
-                Column(
-                    modifier = Modifier
-                        .background(Color.Red).fillMaxWidth(0.5f).fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Chikara ga")
-                    Text(text = "minagite")
-                    Text(text = "kura")
-                }
-
-                Column(
-                    modifier = Modifier
-                        .background(Color.Blue).fillMaxHeight(0.5f),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.End
-                ) {
-                    Text(text = "Chikara ga")
-                    Text(text = "minagite")
-                    Text(text = "kura")
-                }
+            Column(){
+                ListItem(name = "Saitama", prof = "Hero")
+                ListItem(name = "Saitama", prof = "Hero")
+                ListItem(name = "Saitama", prof = "Hero")
+                ListItem(name = "Saitama", prof = "Hero")
+                ListItem(name = "Saitama", prof = "Hero")
+                ListItem(name = "Saitama", prof = "Hero")
+                ListItem(name = "Saitama", prof = "Hero")
             }
-
-
         }
     }
 }
 
+@Composable
+private fun ListItem(name: String, prof: String){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ){
+        Box(){
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.saitama),
+                    contentDescription = "text",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
+    }
+}
