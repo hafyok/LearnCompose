c6eba730f8e85b7546b4c383f804c881070b6b26
Haggai Khafizov <hafyok5777@gmail.com>
2023-07-09 14:41:09 +0300

    Lesson 7: LazyRow

---
+++ app/src/main/java/com/example/myapplication/ItemRowModel.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+data class ItemRowModel(
+    val imageId: Int,
+    val title: String
+)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,59 +1,38 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn (
-                horizontalAlignment = Alignment.CenterHorizontally,
+            LazyRow (
                 modifier = Modifier
-                    .fillMaxSize()
+                    .fillMaxWidth()
+                    .background(Color.Gray)
             ){
                 itemsIndexed(
-                    listOf("item 1", "Item 2", "End", "Finish")
+                    listOf(
+                        ItemRowModel(R.drawable.saitama, "Saitama"),
+                        ItemRowModel(R.drawable.genos, "Genos"),
+                        ItemRowModel(R.drawable.bang, "Bang"),
+                        ItemRowModel(R.drawable.sonic, "Sonic"),
+                        ItemRowModel(R.drawable.king, "King"),
+                        ItemRowModel(R.drawable.garou, "Garou")
+                        )
                 ){_, item ->
-                    Text(
-                        text = item,
-                        fontSize = 30.sp,
-                        modifier = Modifier.padding(vertical = 10.dp)
-                    )
+                    ItemRow(item = item)
                 }
             }
         }
     }
 }
-//LazyColumn и LazyRow намного удобнее RecycleView в XML. Если раньше приходилось создавать
-//целые классы кода для отображения списка, то в Compose все решается чуть ли не пару строк
+//LazyRow то же, что и LazyColumn, но соответственно в строку, а не столбец
